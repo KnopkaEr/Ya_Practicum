a695b6203b530b6fa29a114ca6c396d00112b2ef
KnopkaEr <113869863+KnopkaEr@users.noreply.github.com>
2023-05-07 13:38:16 +0700

    Update functions_diplom.py

--- functions_diplom.py
+++ functions_diplom.py
@@ -2,14 +2,10 @@ import data_diplom
 import urls_diplom
 import requests
 
-
 def created_order(body):
     head = data_diplom.headers_api.copy()
     return requests.post(urls_diplom.URL_USER+urls_diplom.URL_API_CREATE_ORDER, headers=head, json=body)
 
-
 def get_order(track):
     track = str(track)
     return requests.get(urls_diplom.URL_USER+urls_diplom.URL_API_GET_ORDER, params={'t': track})
-
-
